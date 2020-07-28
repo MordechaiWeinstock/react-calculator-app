19b8be742d50108ba288958f3cee3f9be9d530b1
Mordechai Weinstock <gefenemail@gmail.com>
2020-07-28 14:03:44 +0100

    Updates

---
+++ precache-manifest.10682bf721f5d22fa30c56080e64a290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb13269c676360e2a5f301390967604",
+    "url": "/MordechaiWeinstock/react-calculator-app.git/index.html"
+  },
+  {
+    "revision": "0c03f1f27a28e713510f",
+    "url": "/MordechaiWeinstock/react-calculator-app.git/static/css/main.20ab08b7.chunk.css"
+  },
+  {
+    "revision": "8170dfb5d7ddf0f43a61",
+    "url": "/MordechaiWeinstock/react-calculator-app.git/static/js/2.e83e8b22.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/MordechaiWeinstock/react-calculator-app.git/static/js/2.e83e8b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c03f1f27a28e713510f",
+    "url": "/MordechaiWeinstock/react-calculator-app.git/static/js/main.2c0d71f9.chunk.js"
+  },
+  {
+    "revision": "656d27c00e37d928fff6",
+    "url": "/MordechaiWeinstock/react-calculator-app.git/static/js/runtime-main.32184b26.js"
+  }
+]);
